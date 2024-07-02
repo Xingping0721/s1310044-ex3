3a4f6fcc1aaf3f93a0e4f9e95f2ae9ea5aca55b4
Xingping0721 <s1310044@u-aizu.ac.jp>
2024-07-02 09:33:32 +0900

    HoT: initial revision

---
+++ ex3.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
+
+class HeadsOrTails {
+    public static void main(String[] args) {
+         
+        System.out.println("Tossing a coin...");
+
+        Random rand = new Random();
+        int heads = 0;
+        int tails = 0;
+        for (int i = 0; i < 3; i++) {
+
+            int num = rand.nextInt(10) + 100;
+
+            System.out.print("Round "+ (i+1) + ": ");
+            if(num%2 == 0){
+                System.out.println("Heads");
+                heads++;
+            }
+            else{
+                System.out.println("Tails");
+                tails++;
+            }
+        }
+
+        System.out.println("Heads: " + heads + ", Tails: " + tails);
+
+    }
+}
